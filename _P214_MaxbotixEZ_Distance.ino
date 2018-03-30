4e07a6b6590ac07c406691a7108f926cedcd5c08
Stephen Houser <stephenhouser@gmail.com>
2018-03-30 11:45:26 -0400

    update comments, shift to macros for conversions

--- _P214_MaxbotixEZ_Distance.ino
+++ _P214_MaxbotixEZ_Distance.ino
@@ -1,12 +1,59 @@
 //#######################################################################################################
 //#################################### Plugin 214: Maxbotix LV MaxSonar EZ Distance Sensors ##############################################
 //#######################################################################################################
+/*
+ * Plugin to read distance from Maxbotix LV-MaxSonar-EZx series sensors in raw
+ * pulse width, inches, and millimeters.
+ * 
+ * This plugin is designed to use the pulse width representation of range. This 
+ * is Pin 2 on the sensor (LV-MaxSonar-EZ0). The PWM output of the LV-EZx
+ * provides good resolution and is therefore used by this plugin rather than
+ * the serial or analog methods. The serial data would need to be decoded and
+ * thus add unnecessary complexity to the plugin. The resolution of the analog
+ * output seems (via experimentation) to be less precise than the pulse width
+ * method.
+ * 
+ * Pulse Width, from the data sheet:
+ *    Pin 2-PW- This pin outputs a pulse width representation of range. 
+ *    The distance can be calculated using the scale factor of 147uS per inch.
+ * 
+ * This means that:
+ *    inches ~= measured_value / 147.0
+ *      - OR -
+ *    inches ~= measured_value * 0.0068027211
+ * 
+ *    millimeters ~= measured_value / 5.7874015764
+ *      - OR - 
+ *    millimeters ~= measured_value * 0.1727891156
+ * 
+ * Utility macros (below) are provided to convert the raw pulse width to
+ * inches and millimeters using these conversion factors.
+ * 
+ * Currently this plugin "continually ranges" by leaving the sensor's RX pin
+ * (pin 4) unconnected. A future version of this plugin may enable the
+ * "command ranging" option to save power. 
+ * 
+ * Command Ranging, from the data sheet: 
+ *    Pin 4-RX– This pin is internally pulled high. The LV-MaxSonar-EZ will
+ *    continually measure range and output if RX data is left unconnected or 
+ *    held high. If held low the sensor will stop ranging. Bring high for 
+ *    20uS or more to command a range reading.
+ * 
+ * The Maxbotix sensors may partially work with the core plugin `_P103_HCSR04` - 
+ * "Distance - HC-SR04" in the "continually ranging" setup, but the "command
+ * ranging" timing is different, hence this plugin's existence.
+ * 
+ * Product details: https://www.maxbotix.com/Ultrasonic_Sensors.htm#LV-MaxSonar-EZ
+ * Product data sheet: https://www.maxbotix.com/documents/LV-MaxSonar-EZ_Datasheet.pdf
+ * 
+ * Tested with 3.3V on NodeMCU compatible ESP8266.
+ */
 
 #ifdef PLUGIN_BUILD_STEPHEN
 
 #define PLUGIN_214
 #define PLUGIN_ID_214        214
-#define PLUGIN_NAME_214       "Maxbotix LV-EZx - Distance"
+#define PLUGIN_NAME_214       "Distance - Maxbotix LV-EZx"
 #define PLUGIN_VALUENAME1_214 "Distance (raw)"
 #define PLUGIN_VALUENAME2_214 "Distance (inch)"
 #define PLUGIN_VALUENAME3_214 "Distance (mm)"
@@ -19,6 +66,17 @@
 #define PLUGIN_214_CONFIG_SAMPLES 0
 #define PLUGIN_214_CONFIG_DELAY   1
 
+/* Convert pulse width representation of range to inches */
+#define PLUGIN_214_INCH_CONVERSION  147.0
+#define PLUGIN_214_RAW2INCH(value) (value / PLUGIN_214_INCH_CONVERSION);
+
+/* Convert pulse width representation of range to millimeters */
+#define PLUGIN_214_MM_CONVERSION 0.1727891156
+#define PLUGIN_214_RAW2MM(value) (value * PLUGIN_214_MM_CONVERSION);
+
+/*
+ * EPSEasy Plugin
+ */
 boolean Plugin_214(byte function, struct EventStruct *event, String &string)
 {
   switch (function)
@@ -75,8 +133,8 @@ boolean Plugin_214(byte function, struct EventStruct *event, String &string)
         if (value > 0)
         {
           UserVar[event->BaseVarIndex + 0] = value;
-          UserVar[event->BaseVarIndex + 1] = Plugin_214_raw2inch(value);
-          UserVar[event->BaseVarIndex + 2] = Plugin_214_raw2mm(value);
+          UserVar[event->BaseVarIndex + 1] = PLUGIN_214_RAW2INCH(value);
+          UserVar[event->BaseVarIndex + 2] = PLUGIN_214_RAW2MM(value);
           log += UserVar[event->BaseVarIndex];
           success = true;
         }
@@ -150,10 +208,25 @@ boolean Plugin_214(byte function, struct EventStruct *event, String &string)
   return false;
 }
 
+/*
+ * Read raw pulse width of Maxbotix LV-EZx sensor on `data_pin`.
+ * This call is the lowest level of the sensor data collection.
+ * 
+ * Return value is the width of the pulse received (which is proportinal to the distance)
+ */
 float Plugin_214_read(int data_pin) {
   return (float)pulseIn(data_pin, HIGH);
 }
 
+/*
+ * Read Maxbotix LV-EZx sensor multiple times and average the result.
+ * This attempts to mitigate wandering readings by averaging several samples
+ * from the sensor.
+ * 
+ * `data_pin` is the pin the sensor's PWM data is connected to.
+ * `sample_count` is the number of samples (readings) to take.
+ * `sample_delay` is the delay between each sample.
+ */
 float Plugin_214_read_multiple(int data_pin, int sample_count, int sample_delay)
 {
   float value = 0;
@@ -165,14 +238,6 @@ float Plugin_214_read_multiple(int data_pin, int sample_count, int sample_delay)
 
   value /= sample_count;
   return value;
-}
-
-float Plugin_214_raw2inch(float value) {
-  return value / 147.0;
-}
-
-float Plugin_214_raw2mm(float value) {
-  return value * 0.1727891156; // result in mm 
 }
 
 #endif
