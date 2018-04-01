5781bf62bceb8117406183a8c82b6cb4169f27fe
Stephen Houser <stephenhouser@gmail.com>
2018-04-01 10:12:17 -0400

    Change to report back value, min, and max values. Change interface to allow selecting units instead of reporting them all.

--- _P214_MaxbotixEZ_Distance.ino
+++ _P214_MaxbotixEZ_Distance.ino
@@ -39,6 +39,16 @@
  *    held high. If held low the sensor will stop ranging. Bring high for 
  *    20uS or more to command a range reading.
  * 
+ * There tends to be some varaition in readings so the plugin provides for
+ * taking multple readings for each sample, with a delay between individual
+ * readings. The plugin will return the average, minimum, and maximum of these 
+ * readings as it's result. The dafault is to take 10 readings with a 10us
+ * delay between each. This has given me reasonable results. You might need
+ * to adjust based on your sensor and environment.
+ * 
+ * If you get large variability, you might also add a capacitor across Vcc and
+ * GND as described in Maxbotic's troubleshooting tips (see link below).
+ *  
  * The Maxbotix sensors may partially work with the core plugin `_P103_HCSR04` - 
  * "Distance - HC-SR04" in the "continually ranging" setup, but the "command
  * ranging" timing is different, hence this plugin's existence.
@@ -46,7 +56,8 @@
  * Product details: https://www.maxbotix.com/Ultrasonic_Sensors.htm#LV-MaxSonar-EZ
  * Product data sheet: https://www.maxbotix.com/documents/LV-MaxSonar-EZ_Datasheet.pdf
  * 
- * Tested with 3.3V on NodeMCU compatible ESP8266.
+ * Tested with 3.3V and Vcc on NodeMCU compatible ESP8266. I get about the same
+ * results from both power sources.
  */
 
 #ifdef PLUGIN_BUILD_DEV
@@ -54,17 +65,22 @@
 #define PLUGIN_214
 #define PLUGIN_ID_214        214
 #define PLUGIN_NAME_214       "Distance - Maxbotix LV-EZx"
-#define PLUGIN_VALUENAME1_214 "Distance (raw)"
-#define PLUGIN_VALUENAME2_214 "Distance (inch)"
-#define PLUGIN_VALUENAME3_214 "Distance (mm)"
+#define PLUGIN_VALUENAME1_214 "Distance"
+#define PLUGIN_VALUENAME2_214 "Distance (min)"
+#define PLUGIN_VALUENAME3_214 "Distance (max)"
 
 // For Fast/Slow variant
 #define PLUGIN_214_CONFIG_SPEED  0
 // #define PLUGIN_214_SPEED_SLOW 1
 // #define PLUGIN_214_SPEED_FAST 2
 
-#define PLUGIN_214_CONFIG_SAMPLES 0
-#define PLUGIN_214_CONFIG_DELAY   1
+#define PLUGIN_214_CONFIG_UNITS 0
+#define PLUGIN_214_UNITS_RAW    0
+#define PLUGIN_214_UNITS_INCH   1
+#define PLUGIN_214_UNITS_MM     2
+
+#define PLUGIN_214_CONFIG_SAMPLES 1
+#define PLUGIN_214_CONFIG_DELAY   2
 
 /* Convert pulse width representation of range to inches */
 #define PLUGIN_214_INCH_CONVERSION  147.0
@@ -122,26 +138,14 @@ boolean Plugin_214(byte function, struct EventStruct *event, String &string)
 
     case PLUGIN_READ:
       {
-        boolean success = false;
         String log = F("LV-EZx: Distance: ");
-
-        // For custom samples and delay between samples
-        int data_pin = Settings.TaskDevicePin1[event->TaskIndex];
-        int sample_count = Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_SAMPLES];
-        int sample_delay = Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_DELAY];
-        float value = Plugin_214_read_multiple(data_pin, sample_count, sample_delay);
-        if (value > 0)
-        {
-          UserVar[event->BaseVarIndex + 0] = value;
-          UserVar[event->BaseVarIndex + 1] = PLUGIN_214_RAW2INCH(value);
-          UserVar[event->BaseVarIndex + 2] = PLUGIN_214_RAW2MM(value);
+        boolean success = Plugin_214_read_multiple(event);
+        if (success) {
           log += UserVar[event->BaseVarIndex + 0];
-          log += "us ";
+          log += " min=";
           log += UserVar[event->BaseVarIndex + 1];
-          log += "in ";
+          log += " max=";
           log += UserVar[event->BaseVarIndex + 2];
-          log += "mm";
-          success = true;
         }
         else 
         {
@@ -182,18 +186,37 @@ boolean Plugin_214(byte function, struct EventStruct *event, String &string)
         // int optionValues[2] = { PLUGIN_214_SPEED_SLOW, PLUGIN_214_SPEED_FAST };
         // addFormSelector(string, F("Speed"), F("plugin_013_speed"), 2, options, optionValues, choice);
 
-        // For custom samples and delay between samples
-        // Default values; 10 samples, 10us delay between samples
+        // Reporting units (raw pulse width, inches, or mm)
+        // Default value is raw
+        byte units_setting = Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_UNITS];
+        if (units_setting <= 0)
+        {
+          Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_UNITS] = PLUGIN_214_UNITS_RAW;
+        }
+
+        String options[3];
+        options[0] = F("raw");
+        options[1] = F("inches");
+        options[2] = F("mm");
+        int optionValues[3] = { PLUGIN_214_UNITS_RAW, PLUGIN_214_UNITS_INCH, PLUGIN_214_UNITS_MM };
+        addFormSelector(string, F("Units"), F("plugin_214_units"), 3, options, optionValues, units_setting);
+
+        // Number of samples to collect for each reading
+        // Default is 10 samples per reading
         if (Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_SAMPLES] <= 0)
         {
           Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_SAMPLES] = 10;
         }
+
         addFormNumericBox(string, F("Samples"), F("plugin_214_samples"), Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_SAMPLES]);
 
+        // Delay between samples
+        // Default is 10us
         if (Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_DELAY] <= 0)
         {
           Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_DELAY] = 10;
         }
+
         addFormNumericBox(string, F("Delay"), F("plugin_214_delay"), Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_DELAY]);
         
         return true;
@@ -203,8 +226,7 @@ boolean Plugin_214(byte function, struct EventStruct *event, String &string)
       {
         // For Fast/Slow variant
         // Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_SPEED] = getFormItemInt(F("plugin_013_speed"));
-
-        // For custom samples and delay between samples
+        Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_UNITS] = getFormItemInt(F("plugin_214_units"));
         Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_SAMPLES] = getFormItemInt(F("plugin_214_samples"));
         Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_DELAY] = getFormItemInt(F("plugin_214_delay"));
         return true;
@@ -214,35 +236,93 @@ boolean Plugin_214(byte function, struct EventStruct *event, String &string)
 }
 
 /*
- * Read raw pulse width of Maxbotix LV-EZx sensor on `data_pin`.
- * This call is the lowest level of the sensor data collection.
- * 
- * Return value is the width of the pulse received (which is proportinal to the distance)
- */
-float Plugin_214_read(int data_pin) {
-  return (float)pulseIn(data_pin, HIGH);
-}
-
-/*
  * Read Maxbotix LV-EZx sensor multiple times and average the result.
  * This attempts to mitigate wandering readings by averaging several samples
  * from the sensor.
+ * 
+ * Data is returned directly into the plugin's three value fields.
+ * BaseVarIndex + 0 = the averaged pulse width reading
+ * BaseVarIndex + 1 = the minimum pulse width reading
+ * BaseVarIndex + 2 = the maximum pulse width reading
  * 
  * `data_pin` is the pin the sensor's PWM data is connected to.
  * `sample_count` is the number of samples (readings) to take.
  * `sample_delay` is the delay between each sample.
  */
-float Plugin_214_read_multiple(int data_pin, int sample_count, int sample_delay)
+boolean Plugin_214_read_multiple(struct EventStruct *event)
 {
-  float value = 0;
-  for (int i = 0; i < sample_count; i++)
-  {
-    value += Plugin_214_read(data_pin);
-    delay(sample_delay);
+  int pulse = 0;
+  int pulse_sum = -1;
+  int pulse_min = -1;
+  int pulse_max = -1;
+
+  int data_pin = Settings.TaskDevicePin1[event->TaskIndex];
+  int sample_count = Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_SAMPLES];
+  int sample_delay = Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_DELAY];
+
+  // sample_count must be > 0 to prevent divide by zero
+  // sample_delay must be >= 0 to prevent negative delay
+  if (sample_count <= 0 || sample_delay <= 0) {
+    addLog(LOG_LEVEL_INFO, "LV-EZ: Samples and Sample Delay must be non-negative.");
+    return false;
   }
 
-  value /= sample_count;
-  return value;
+  for (int i = 0; i < sample_count; i++)
+  {
+    // This is where the action happens. Read a pulse width
+    pulse = pulseIn(data_pin, HIGH);
+
+    // accumulate pulse widths to compute final value
+    pulse_sum += pulse;
+
+    // Compute min and max readings for this run
+    if (pulse_min < 0 || pulse < pulse_min) {
+      pulse_min = pulse;
+    }
+    if (pulse_max < 0 || pulse > pulse_max) {
+      pulse_max = pulse;
+    }
+
+    // Delay, if we are delaying between readings.
+    if (sample_delay > 0) {
+      delay(sample_delay);
+    }
+  }
+
+  // Return success only if there is a (positive) sum of pulses to average
+  if (pulse_sum >= 0)
+  {
+    // Compute average pulse width reading
+    float value = (float)pulse_sum / (float)sample_count;
+
+    // convert values based on units selection
+    // save values to the plugin result fields
+    switch (Settings.TaskDevicePluginConfig[event->TaskIndex][PLUGIN_214_CONFIG_UNITS]) 
+    {
+      case PLUGIN_214_UNITS_INCH:
+        UserVar[event->BaseVarIndex + 0] = PLUGIN_214_RAW2INCH(value);
+        UserVar[event->BaseVarIndex + 1] = PLUGIN_214_RAW2INCH((float)pulse_min);
+        UserVar[event->BaseVarIndex + 2] = PLUGIN_214_RAW2INCH((float)pulse_max);
+        break;
+
+      case PLUGIN_214_UNITS_MM:
+        UserVar[event->BaseVarIndex + 0] = PLUGIN_214_RAW2MM(value);
+        UserVar[event->BaseVarIndex + 1] = PLUGIN_214_RAW2MM((float)pulse_min);
+        UserVar[event->BaseVarIndex + 2] = PLUGIN_214_RAW2MM((float)pulse_max);
+        break;
+
+      case PLUGIN_214_UNITS_RAW:
+      default:
+        UserVar[event->BaseVarIndex + 0] = (float)value;
+        UserVar[event->BaseVarIndex + 1] = (float)pulse_min;
+        UserVar[event->BaseVarIndex + 2] = (float)pulse_max;
+        break;
+    }
+
+    return true;
+  }
+
+  return false;
 }
 
 #endif
